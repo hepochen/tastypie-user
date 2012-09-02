b0a2995304f51fee7fc2992d44a3ccb6dba9fab6
Hepo <hepochen@gmail.com>
2012-09-02 21:07:33 +0800

    pep8

--- tastypie_user/models.py
+++ tastypie_user/models.py
@@ -2,26 +2,28 @@
 import warnings
 from django.conf import settings
 from django.contrib.auth.models import User
-from tastypie_user.utils import send_email,load_email_content
+from tastypie_user.utils import send_email, load_email_content
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import int_to_base36
 
-class ExtendedUserMethods:
-    def send_email(self, action_type, ctx_dict={}, from_email=None):
+
+class ExtendedUserMethods:  # (Users)
+    def send_email(self, action_type, ctx_dict=None, from_email=None):
+        ctx_dict = ctx_dict or {}
         ctx_dict.update({
             'uid': int_to_base36(self.id),
             'token': default_token_generator.make_token(self),
         })
 
         if not self.email:
-            warnings.warn('uid:%s has no email address'%self.id)
+            warnings.warn('uid:%s has no email address' % self.id)
             return
 
         if from_email is None:
             from_email = settings.DEFAULT_FROM_EMAIL
 
-        subject, message, content_subtype = load_email_content(action_type,ctx_dict)
-        send_email(subject, message, from_email, self.email, content_subtype)
+        subject, message, subtype = load_email_content(action_type, ctx_dict)
+        send_email(subject, message, from_email, self.email, subtype)
 
 
 User.__bases__ += (ExtendedUserMethods, )
