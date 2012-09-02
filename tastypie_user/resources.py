6feecb193e24643246ad8934964c2ab75cdc0387
Hepo <hepochen@gmail.com>
2012-09-02 21:08:27 +0800

    add api_key auto change

--- tastypie_user/resources.py
+++ tastypie_user/resources.py
@@ -13,15 +13,35 @@ from django.conf import settings
 from django.utils.http import base36_to_int
 from django.contrib.auth.models import User
 from tastypie.authorization import Authorization
-from tastypie_user.utils import lazy_import
+from tastypie_user.utils import lazy_import, datetime_now
 from tastypie import http
 
 
-UserCreationForm = lazy_import(
-    getattr(settings, 'TASTYPIE_USER_CREATION_FORM', 'django.contrib.auth.forms.UserCreationForm')
+USER_CREATION_FORM = lazy_import(
+    getattr(
+        settings,
+        'TASTYPIE_USER_CREATION_FORM',
+        'django.contrib.auth.forms.UserCreationForm'
+    )
 )
-CAN_CHANGE_UNUSABLE_PASSWORD = getattr(settings, 'CAN_CHANGE_UNUSABLE_PASSWORD', True)
-AUTO_LOGIN_AFTER_RESET_PASSWORD = getattr(settings, 'AUTO_LOGIN_AFTER_RESET_PASSWORD', True)
+CAN_CHANGE_UNUSABLE_PASSWORD = getattr(
+    settings,
+    'CAN_CHANGE_UNUSABLE_PASSWORD',
+    True)
+AUTO_LOGIN_AFTER_RESET_PASSWORD = getattr(
+    settings,
+    'AUTO_LOGIN_AFTER_RESET_PASSWORD',
+    True
+)
+MIN_PASSWORD_LENGTH = getattr(settings, 'MIN_PASSWORD_LENGTH', 6)
+DEFAULT_USER_BACKEND = 'django.contrib.auth.backends.ModelBackend'
+
+
+def change_api_key(user):
+    api_key = user.api_key
+    api_key.key = api_key.generate_key()
+    api_key.created = datetime_now()
+    api_key.save()
 
 def change_password(user, new_password):
     if not user.is_authenticated():
@@ -30,26 +50,29 @@ def change_password(user, new_password):
     if new_password == UNUSABLE_PASSWORD and not CAN_CHANGE_UNUSABLE_PASSWORD:
         raise BadRequest('not allowed to change blank password')
 
-    if new_password:
+    if new_password and len(new_password) < MIN_PASSWORD_LENGTH:
+        raise BadRequest(
+            'password should be longer than %s' % MIN_PASSWORD_LENGTH)
+    elif not new_password:
+        raise BadRequest('password is not allowed blank')
+    else:
         user.set_password(new_password)
         user.save()
-    else:
-        raise BadRequest('no password filled')
+        change_api_key(user)
 
 
 class UserResource(ModelResource):
 
-    def show_keys(self,request):
+    def show_keys(self, request):
         if request.user.is_authenticated():
             api_key = request.user.api_key.key
             keys = {
                 'username': request.user.username,
                 'api_key': api_key,
                 'session_name': settings.SESSION_COOKIE_NAME,
-                'session_key': request.session.session_key,
-                }
+                'session_key': request.session.session_key}
             raise ImmediateHttpResponse(
-                HttpResponse(json.dumps(keys),content_type='application/json')
+                HttpResponse(json.dumps(keys), content_type='application/json')
             )
         else:
             raise BadRequest('not login')
@@ -58,7 +81,7 @@ class UserResource(ModelResource):
         create_type = bundle.data.pop('type', 'register')
 
         if create_type == 'register':
-            form = UserCreationForm(bundle.data)
+            form = USER_CREATION_FORM(bundle.data)
             if form.is_valid():
                 new_user = form.save()
                 new_user.send_email('activate')
@@ -86,23 +109,25 @@ class UserResource(ModelResource):
             raise BadRequest('create user resource error')
 
     def get_detail(self, request=None, **kwargs):
-        if kwargs.get('pk') == 'me':
+        if kwargs.get('pk') == 'keys':
             self.show_keys(request)
         else:
-            return super(UserResource,self).get_detail(request,**kwargs)
+            return super(UserResource, self).get_detail(request, **kwargs)
 
     def patch_detail(self, request, **kwargs):
-        data = json.loads(request.META.get('data',request.raw_post_data))
-        action_type = data.pop('action',None)
-        if action_type:#triggerre by action/function
+        data = json.loads(request.META.get('data', request.raw_post_data))
+        action_type = data.pop('action', None)
+
+        if action_type:  # triggerre by action/function
             if action_type == 'change_password':
                 change_password(request.user, data.get('new_password'))
+                self.show_keys(request)
 
             elif action_type == 'request_reset_password':
                 try:
                     email = data.get('email')
                     if not email:
-                        raise BadRequest('need email address to reset password')
+                        raise BadRequest('need email to reset password')
                     user = User.objects.get(email=email)
                     user.send_email('reset_password')
                 except User.DoesNotExist:
@@ -125,8 +150,10 @@ class UserResource(ModelResource):
                     change_password(user, data.get('new_password'))
 
                     if AUTO_LOGIN_AFTER_RESET_PASSWORD:
-                        user.backend = 'django.contrib.auth.backends.ModelBackend'
-                        auth.login(request,user)
+                        user.backend = DEFAULT_USER_BACKEND
+                        auth.login(request, user)
+
+                    self.show_keys(request)
                 else:
                     raise BadRequest('token error can not reset your password')
 
@@ -136,18 +163,21 @@ class UserResource(ModelResource):
             #at last
             raise ImmediateHttpResponse(http.HttpAccepted())
 
+        if not action_type and request.user.is_authenticated():
+            return super(UserResource, self).patch_detail(request, **kwargs)
         else:
-            if request.user.is_authenticated():
-                return super(UserResource,self).patch_detail(request,**kwargs)
-            else:
-                raise BadRequest('can not modify info of other user')
-
+            raise BadRequest('can not modify info of other user')
 
     def obj_delete(self, request=None, **kwargs):
         delete_type = kwargs.get('pk')
 
+        if not request.user.is_authenticated():
+            raise BadRequest('delete a resource, you should login first')
+
         if delete_type == 'session':
             auth.logout(request)
+        elif delete_type == 'api_key':
+            change_api_key(request.user)
         else:
             raise BadRequest('This delete type is not allowed')
 
@@ -160,7 +190,6 @@ class UserResource(ModelResource):
     def obj_get_list(self, request=None, **kwargs):
         raise BadRequest('not allowed')
 
-
     def dehydrate(self, bundle):
         bundle.data['email'] = ''
         bundle.data['password'] = ''
@@ -169,7 +198,7 @@ class UserResource(ModelResource):
     class Meta:
         queryset = User.objects.filter()
         resource_name = 'user'
-        detail_allowed_methods = ['get','patch','put','delete']
-        list_allowed_methods = ['get','post','delete','patch']
-        fields = ['first_name','last_name']
+        detail_allowed_methods = ['get', 'patch', 'put', 'delete']
+        list_allowed_methods = ['get', 'post', 'delete', 'patch']
+        fields = ['first_name', 'last_name']
         authorization = Authorization()
