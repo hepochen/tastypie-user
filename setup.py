cb9a1485dcac1d502ca607aa7e4b938f505d077c
Hepo <hepochen@gmail.com>
2012-09-02 21:08:55 +0800

    remove to fox and setup.py it!

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+''' Setup File for backend '''
+from setuptools import setup
+
+setup(
+    name="tastypie-user",
+    version="0.0.1",
+    description='A RESTful APIs for User resource',
+    author="Hepochen",
+    author_email="hepochen@gmail.com",
+    url="https://github.com/hepochen/",
+    packages=[
+        'tastypie_user', ],
+    long_description="Please see README.me"
+)
